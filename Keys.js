a37ccd8ceed1e268fa1c067fe34f08791dc8b20e
Dave Shaw <dave@taeguk.co.uk>
2016-07-08 17:10:01 +0100

    Initial Version

---
+++ Keys.js
@@ -0,0 +1,51 @@
+/// <reference path="refs/jquery.d.ts"/>
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function getNewValue() {
+    var randomChar = randomIntFromInterval(65, 68);
+    //return String.fromCharCode(randomChar);
+    var newV = String.fromCharCode(randomChar);
+    if (currentValue === newV) {
+        return getNewValue();
+    }
+    else {
+        return newV;
+    }
+}
+//Functions above this line do not change state!
+var timer;
+var currentValue;
+function setNewValue(value) {
+    var faderSpeed = 400;
+    var changer = $("#changer");
+    changer.fadeOut(faderSpeed, function () {
+        currentValue = value;
+        changer.text(value);
+        changer.fadeIn(faderSpeed, function () {
+            //Nothing ATM
+        });
+    });
+}
+function changeLoop(x) {
+    if (x === 0) {
+        return;
+    }
+    clearTimeout(timer);
+    var y = x;
+    if (y > 0) {
+        var newValue = getNewValue();
+        setNewValue(newValue);
+        timer = setTimeout(function () {
+            changeLoop(y);
+        }, 5000);
+        y--;
+    }
+}
+function hit() {
+    clearTimeout(timer);
+}
+function start() {
+    changeLoop(20);
+}
+//# sourceMappingURL=Keys.js.map
---
+++ Keys.ts
@@ -0,0 +1,64 @@
+/// <reference path="refs/jquery.d.ts"/>
+
+function randomIntFromInterval(min : number, max : number)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function getNewValue() {
+    var randomChar = randomIntFromInterval(65, 68);
+    //return String.fromCharCode(randomChar);
+    var newV = String.fromCharCode(randomChar);
+    if (currentValue === newV) {
+        return getNewValue();
+    }
+    else {
+        return newV;
+    }
+}
+
+//Functions above this line do not change state!
+
+var timer : number;
+var currentValue : string;
+
+function setNewValue(value: string) {
+    var faderSpeed = 400;
+
+    var changer = $("#changer");
+    changer.fadeOut(faderSpeed, function () {
+        currentValue = value;
+    	changer.text(value);
+        changer.fadeIn(faderSpeed, function() {
+            //Nothing ATM
+        })
+    });
+}
+
+function changeLoop(x : number) {
+    if (x === 0) {
+        return;
+    }
+
+    clearTimeout(timer);
+
+    var y = x;
+
+    if (y > 0) {
+        var newValue = getNewValue();
+        setNewValue(newValue);
+
+        timer = setTimeout(function() {
+           changeLoop(y); 
+        }, 5000);
+        y--;    
+    }
+}
+
+function hit() {
+    clearTimeout(timer);
+}
+
+function start() {
+    changeLoop(20);
+}
