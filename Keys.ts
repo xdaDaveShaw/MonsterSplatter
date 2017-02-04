da0062763052ee49b5b20619d5871b0806a5f1c8
Dave Shaw <dave@taeguk.co.uk>
2017-02-04 17:53:10 +0000

    Fix some TSLint complaints

--- Keys.ts
+++ Keys.ts
@@ -1,70 +1,69 @@
 /// <reference path="refs/jquery.d.ts"/>
 /// <reference path="refs/jqueryui.d.ts"/>
 
-const debugToConsole = false;
+const debugToConsole: boolean = false;
 
-function log(message: string) {
+function log(message: string): void {
     if (debugToConsole) {
         console.log(message);
     }
 }
 
-function randomIntFromInterval(min: number, max: number) {
+function randomIntFromInterval(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function getNewValue(): string {
-    let randomChar = randomIntFromInterval(65, 68);
+    let randomChar: number  = randomIntFromInterval(65, 68);
     // return String.fromCharCode(randomChar);
-    // Temp to ensure non-sequential values
-    let newV = String.fromCharCode(randomChar);
+    // temp to ensure non-sequential values
+    let newV: string = String.fromCharCode(randomChar);
     if (currentValue === newV) {
         return getNewValue();
-    }
-    else {
+    } else {
         return newV;
     }
 }
 
-function enableButton(selector: string) {
+function enableButton(selector: string): void {
     $(selector).removeAttr("disabled");
 }
 
-function disableButton(selector: string) {
+function disableButton(selector: string): void {
     $(selector).prop("disabled", true);
 }
 
-// Functions above this line do not change state!
+// functions above this line do not change state!
 
-const secondsInAGame = 30; //Total length of the game in seconds
-const tickLengthInMs = 250; //Each tick is a countdown on the timer.
-const valueDisplayedForInMs = 1500; //How long you have to think about the match.
+const secondsInAGame: number = 30;          // total length of the game in seconds
+const tickLengthInMs: number = 250;         // each tick is a countdown on the timer.
+const valueDisplayedForInMs: number = 1500; // how long you have to think about the match.
 
 let countdownTimer: number;
 let currentValue: string;
 let currentTarget: string;
 let currentScore: number = 0;
-let canHit = false;
+let canHit: boolean = false;
 let ticksRemainingInGame: number;
 let elapsedTicks: number = 0;
 
-function setScore(score: number) {
+function setScore(score: number): void {
     currentScore = score;
     $("#score").text(score);
 }
 
-function incrementScore() {
+function incrementScore(): void  {
     log("incrementing score. current score: " + currentScore);
     setScore(currentScore + 5);
 }
 
-function setNewValue(firstValue: boolean = false) {
-    const faderSpeed = 300;
+function setNewValue(firstValue: boolean = false): void {
+    const faderSpeed: number = 300;
 
     if (ticksRemainingInGame > 0) {
-        let value = getNewValue();
-        let changer = $("#changer");
-        changer.fadeOut(firstValue ? 0 : faderSpeed, function () {
+        let value: string = getNewValue();
+        let changer: JQuery = $("#changer");
+        changer.fadeOut(firstValue ? 0 : faderSpeed, function (): void {
             changer.text(value);
             currentValue = value;
             log("set current value to: " + value);
@@ -75,7 +74,7 @@ function setNewValue(firstValue: boolean = false) {
     }
 }
 
-function hit() {
+function hit(): void {
     log("hit called: canHit : " + canHit + " currentValue: " + currentValue);
 
     if (canHit && currentTarget === currentValue) {
@@ -88,24 +87,23 @@ function hit() {
     }
 }
 
-function stopGame() {
+function stopGame(): void {
     enableButton("#start");
     disableButton("#hit");
     clearTimeout(countdownTimer);
 }
 
-function countdownTime() {
+function countdownTime(): void {
     if (ticksRemainingInGame <= 0) {
         log("Stopped: " + new Date().toISOString());
         stopGame();
-    }
-    else {
+    } else {
         ticksRemainingInGame--;
         log("ticksRemaining: " + ticksRemainingInGame);
 
         clearTimeout(countdownTimer);
 
-        countdownTimer = setTimeout(function () {
+        countdownTimer = setTimeout(function (): void {
             if (elapsedTicks * tickLengthInMs >= valueDisplayedForInMs) {
                 elapsedTicks = 0;
                 setNewValue();
@@ -118,7 +116,7 @@ function countdownTime() {
     $("#progressBar").progressbar("option", "value", ticksRemainingInGame);
 }
 
-function start() {
+function start(): void {
     disableButton("#start");
     setScore(0);
 
@@ -140,8 +138,8 @@ function start() {
     countdownTime();
 }
 
-document.onkeypress = function (e) {
-    if (e.keyCode === 72 || e.keyCode === 104) { //H (maybe?)
+document.onkeypress = function (e: KeyboardEvent): void {
+    if (e.keyCode === 72 || e.keyCode === 104) { // h (maybe?)
         hit();
     }
 };
