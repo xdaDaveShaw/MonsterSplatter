5c5e51d8c6bdbcf02b593e1c2e3f9fd759084d36
Dave Shaw <dave@taeguk.co.uk>
2018-07-25 01:33:05 +0100

    Stripping Down

--- src/App.fs
+++ src/App.fs
@@ -9,40 +9,13 @@ open Fable.Import
 open Fable.Import.Browser
 open Types
 open App.State
-open Global
 
 importAll "../sass/main.sass"
 
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
-let menuItem label page currentPage =
-    li
-      [ ]
-      [ a
-          [ classList [ "is-active", page = currentPage ]
-            Href (toHash page) ]
-          [ str label ] ]
-
-let menu currentPage =
-  aside
-    [ ClassName "menu" ]
-    [ p
-        [ ClassName "menu-label" ]
-        [ str "General" ]
-      ul
-        [ ClassName "menu-list" ]
-        [ menuItem "Home" Home currentPage
-          menuItem "Counter sample" Counter currentPage
-          menuItem "About" Page.About currentPage ] ]
-
 let root model dispatch =
-
-  let pageHtml =
-    function
-    | Page.About -> Info.View.root
-    | Counter -> Counter.View.root model.counter (CounterMsg >> dispatch)
-    | Home -> Home.View.root model.home (HomeMsg >> dispatch)
 
   div
     []
@@ -50,7 +23,7 @@ let root model dispatch =
         [ ClassName "navbar-bg" ]
         [ div
             [ ClassName "container" ]
-            [ Navbar.View.root ] ]
+            [] ]
       div
         [ ClassName "section" ]
         [ div
@@ -59,10 +32,10 @@ let root model dispatch =
                 [ ClassName "columns" ]
                 [ div
                     [ ClassName "column is-3" ]
-                    [ menu model.currentPage ]
+                    []
                   div
                     [ ClassName "column" ]
-                    [ pageHtml model.currentPage ] ] ] ] ]
+                    [] ] ] ] ]
 
 open Elmish.React
 open Elmish.Debug
@@ -70,8 +43,8 @@ open Elmish.HMR
 
 // App
 Program.mkProgram init update root
-|> Program.toNavigable (parseHash pageParser) urlUpdate
 #if DEBUG
+|> Program.withConsoleTrace
 |> Program.withDebugger
 |> Program.withHMR
 #endif
--- src/Types.fs
+++ src/Types.fs
@@ -1,13 +1,14 @@
 module App.Types
 
-open Global
-
 type Msg =
-  | CounterMsg of Counter.Types.Msg
-  | HomeMsg of Home.Types.Msg
+  | NewMonster of string
+  | HitPressed
 
 type Model = {
-    currentPage: Page
-    counter: Counter.Types.Model
-    home: Home.Types.Model
+    TargetMonster: string
+    CurrentMonster: string
+    Score: int
+    //HasHitBefore
+    //Game State
+    //HighScore
   }
