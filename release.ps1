b3cf736b768f5385774621ff955652020b6cd1ab
Dave Shaw <dave@taeguk.co.uk>
2018-11-18 21:34:24 +0000

    Comments on Release script

--- release.ps1
+++ release.ps1
@@ -4,18 +4,23 @@ yarn install
 
 Set-Location -Path .\src
 
+# Production build the site *into public
 dotnet fable yarn-build
 
 Set-Location ..
 
+# Copy the site to a folder not under source control so it won't be removed on Checkout of GH Pages.
 Copy-Item .\public\ -Filter *.* -Destination .\release\ -Recurse
 
 git checkout gh-pages
 
+# Now put the site in the root of the repo.
 Get-ChildItem .\release | Copy-Item -Destination . -Recurse
 
+# And sack of the copy
 Remove-Item .\release -Recurse
 
-#git checkout fable
+# Now start Bash with Jekyll to test the site on.
+bash -c "jekyll server"
 
-
+#git checkout master
